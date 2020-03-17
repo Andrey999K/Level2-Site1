cd250442dd464a0423a7060649b3daf319865e02
Andrey999K <andrey1234xxxx@mail.ru>
2020-03-17 11:07:36 +0300

    update 1

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-	// Custom JS
-	console.log('Hello, World!');
+	const menu_ = document.querySelector('.nav-list');
+	const scrollspy = new VanillaScrollspy(menu_);
+	scrollspy.init();
+
+	window.addEventListener('scroll', () => {
+		let y = window.pageYOffset;
+		const header = document.querySelectorAll(".header");
+
+		if (y > 0) {
+			header[0].classList.add('scroll');
+		} else {
+			header[0].classList.remove('scroll');
+		}
+	});
 });
