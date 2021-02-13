00ce5e252d2c7dd7fce7c2659c270348e1b9a195
Andrey999K <andrey1234xxxx@mail.ru>
2021-02-13 15:13:28 +0300

    add: edit language nav

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,6 +3,14 @@ document.addEventListener("DOMContentLoaded", () => {
 	const scrollspy = new VanillaScrollspy(menu_);
 	scrollspy.init();
 
+	const languages = document.querySelectorAll(".header__lang button");
+	const navList = document.querySelectorAll(".nav-list a");
+	const h2 = document.querySelectorAll("h2");
+	const h3 = document.querySelectorAll("h3");
+	const p = document.querySelectorAll("p");
+
+	let languageTexts = [];
+
 	window.addEventListener('scroll', () => {
 		let y = window.pageYOffset;
 		const header = document.querySelectorAll(".header");
@@ -13,4 +21,35 @@ document.addEventListener("DOMContentLoaded", () => {
 			header[0].classList.remove('scroll');
 		}
 	});
+
+	const getData = async function (url) {
+        const response = await fetch(url);
+
+        if (!response.ok) {
+            throw new Error(
+                `Ошибка по адресу ${url}, статус ошибки ${response.status}!`
+            );
+        }
+        return await response.json();
+    };
+
+	getData("data/languages.json").then(function (data) {
+        languageTexts = data;
+    });
+
+	//СМЕНА ЯЗЫКА
+	languages[0].addEventListener("click", () => {
+		console.log("English");
+		navList.forEach((item, index) => {
+			item.textContent = languageTexts[0].nav[index];
+		});
+	});
+
+	languages[1].addEventListener("click", () => {
+		console.log("Русский");
+		navList.forEach((item, index) => {
+			item.textContent = languageTexts[1].nav[index];
+		});
+	});
+
 });
--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -17,5 +17,9 @@ module.exports = function () {
             .pipe($.plugins.sourcemaps.write('./')) 
             .pipe($.gulp.dest('./build/scripts'));
     });
-    $.gulp.task('scripts', $.gulp.series('scripts-libs', 'scripts-main'));
+    $.gulp.task('json', () => {
+        return $.gulp.src(['./app/data/*.json'])
+            .pipe($.gulp.dest('./build/data'));
+    });
+    $.gulp.task('scripts', $.gulp.series('json', 'scripts-libs', 'scripts-main'));
 };
