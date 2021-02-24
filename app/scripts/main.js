ed5b53ab27af133909cf47665cf6d8dbd833b83a
Andrey999K <andrey1234xxxx@mail.ru>
2021-02-24 21:55:13 +0300

    add: wrapper, rewrote the code

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,31 +1,38 @@
 document.addEventListener("DOMContentLoaded", () => {
-	const menu_ = document.querySelector('.header');
-	const scrollspy = new VanillaScrollspy(menu_);
-	scrollspy.init();
+    const menu_ = document.querySelector('.header');
+    const scrollspy = new VanillaScrollspy(menu_);
+    scrollspy.init();
 
-	const languages = document.querySelectorAll(".header__lang button");
-	const navList = document.querySelectorAll(".nav-list a");
-	const h2 = document.querySelectorAll("h2");
-	const h3 = document.querySelectorAll("h3");
-	const p = document.querySelectorAll("p:not(form p)");
-	const inputs = document.querySelectorAll("form input");
-	const buttons = document.querySelectorAll("button:not(.lang)");
-	const prise = document.querySelectorAll(".direction-list__prise");
+    new Swiper('.reviews__slider', {
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev'
+        }
+    });
 
-	let languageTexts = [];
+    const languages = document.querySelectorAll(".header__lang button");
+    const navList = document.querySelectorAll(".nav-list a");
+    const h2 = document.querySelectorAll("h2");
+    const h3 = document.querySelectorAll("h3");
+    const p = document.querySelectorAll("p:not(form p)");
+    const inputs = document.querySelectorAll("form input");
+    const buttons = document.querySelectorAll("button:not(.lang)");
+    const prise = document.querySelectorAll(".direction-list__prise");
 
-	window.addEventListener('scroll', () => {
-		let y = window.pageYOffset;
-		const header = document.querySelectorAll(".header");
+    let languageTexts = [];
 
-		if (y > 0) {
-			header[0].classList.add('scroll');
-		} else {
-			header[0].classList.remove('scroll');
-		}
-	});
+    window.addEventListener('scroll', () => {
+        let y = window.pageYOffset;
+        const header = document.querySelectorAll(".header");
 
-	const getData = async function (url) {
+        if (y > 0) {
+            header[0].classList.add('scroll');
+        } else {
+            header[0].classList.remove('scroll');
+        }
+    });
+
+    const getData = async function (url) {
         const response = await fetch(url);
 
         if (!response.ok) {
@@ -36,45 +43,45 @@ document.addEventListener("DOMContentLoaded", () => {
         return await response.json();
     };
 
-	getData("data/languages.json").then(function (data) {
+    getData("data/languages.json").then(function (data) {
         languageTexts = data;
     });
 
-	function replaceWords(mass, lang, name) {
-		mass.forEach((item, index) => {
-			item.textContent = languageTexts[lang][name][index];
-		});
-	}
+    function replaceWords(mass, lang, name) {
+        mass.forEach((item, index) => {
+            item.textContent = languageTexts[lang][name][index];
+        });
+    }
 
-	//СМЕНА ЯЗЫКА
-	languages[0].addEventListener("click", () => {
-		console.log("English");
-		replaceWords(navList, 0, "nav");
-		replaceWords(h2, 0, "h2");
-		replaceWords(h3, 0, "h3");
-		replaceWords(p, 0, "p");
-		inputs.forEach((item, index) => {
-			item.placeholder = languageTexts[0].placeholders[index];
-		});
-		replaceWords(buttons, 0, "buttons");
-		prise.forEach(item => {
-			item.innerHTML = item.innerHTML.replace("от", "from");
-		});
-	});
+    //СМЕНА ЯЗЫКА
+    languages[0].addEventListener("click", () => {
+        console.log("English");
+        replaceWords(navList, 0, "nav");
+        replaceWords(h2, 0, "h2");
+        replaceWords(h3, 0, "h3");
+        replaceWords(p, 0, "p");
+        inputs.forEach((item, index) => {
+            item.placeholder = languageTexts[0].placeholders[index];
+        });
+        replaceWords(buttons, 0, "buttons");
+        prise.forEach(item => {
+            item.innerHTML = item.innerHTML.replace("от", "from");
+        });
+    });
 
-	languages[1].addEventListener("click", () => {
-		console.log("Русский");
-		replaceWords(navList, 1, "nav");
-		replaceWords(h2, 1, "h2");
-		replaceWords(h3, 1, "h3");
-		replaceWords(p, 1, "p");
-		inputs.forEach((item, index) => {
-			item.placeholder = languageTexts[1].placeholders[index];
-		});
-		replaceWords(buttons, 1, "buttons");
-		prise.forEach(item => {
-			item.innerHTML = item.innerHTML.replace("from", "от");
-		});
-	});
+    languages[1].addEventListener("click", () => {
+        console.log("Русский");
+        replaceWords(navList, 1, "nav");
+        replaceWords(h2, 1, "h2");
+        replaceWords(h3, 1, "h3");
+        replaceWords(p, 1, "p");
+        inputs.forEach((item, index) => {
+            item.placeholder = languageTexts[1].placeholders[index];
+        });
+        replaceWords(buttons, 1, "buttons");
+        prise.forEach(item => {
+            item.innerHTML = item.innerHTML.replace("from", "от");
+        });
+    });
 
 });
