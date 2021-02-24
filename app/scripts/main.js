142803ba297b72aa5ecc505da105fdc0b0cf7755
Andrey999K <andrey1234xxxx@mail.ru>
2021-02-24 23:20:22 +0300

    fix: slider in review section

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,6 +7,10 @@ document.addEventListener("DOMContentLoaded", () => {
         navigation: {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev'
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
         }
     });
 
